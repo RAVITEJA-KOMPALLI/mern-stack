6b9005c48cbacec14ab8435f7ddf8e6cc210e513
raviteja kompalli <ravitejakompalli01@gmail.com>
2023-02-15 22:10:45 +0530

    Card and Mongo Connect

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
+
+const options = {
+	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
+	index:['index.html'],
+}
+app.use(express.static("images",options));
 
 app.get("/", function(req, res){
 	// res.send("I'm Raviteja");
@@ -9,8 +18,19 @@ app.get("/", function(req, res){
 app.get("/resume", function(req, res){
 	res.sendFile(__dirname + "/resume.html")
 })
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+app.get("/card", function(req, res){
+	res.sendFile(__dirname + "/card.html")
+})
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err){
+	if(err){
+		console.error(err)
+	}else{
+		console.log("DB Connected")
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	}
 });
+
